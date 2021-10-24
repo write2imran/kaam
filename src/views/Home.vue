84dd0edf7098ef22ca3ed1a1c7621fe9ef3320ce
Imran Rauf <write2imran@yahoo.com>
2021-10-24 15:18:45 +0300

    Project GUI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import SingleProject from '../components/SingleProject.vue'
 
 export default {
-  name: "Home",
-  components: {},
-};
+  name: 'Home',
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err))
+  },
+}
 </script>
